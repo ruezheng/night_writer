32e24c02b8d4b3fdb505090d317ee9810adbd495
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-23 12:05:26 -0600

    Fix: add attribute to print_braille method to return any specified letter

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -3,22 +3,22 @@ class Dictionary
 
   def initialize
     @letters_hash = {
-      a: %w[0. .. ..]#,
-      # b: %w[0. 0. ..],
-      # c: %w[00 .. ..]
+      a: %w[0. .. ..],
+      b: %w[0. 0. ..],
+      c: %w[00 .. ..]
     }
   end
 
-  def print_braille
+  def print_braille(letter)
     braille = ""
     @letters_hash.each do |key, value|
-      row0 = value[0]
-      row1 = value[1]
-      row2 = value[2]
-      braille = "#{row0}\n" + "#{row1}\n" + "#{row2}"
+      if letter == key
+        row0 = value[0]
+        row1 = value[1]
+        row2 = value[2]
+        braille = "#{row0}\n" + "#{row1}\n" + "#{row2}"
+      end
     end
     braille
   end
-  # value[1].delete("")
-  # value[2].delete("")
 end
