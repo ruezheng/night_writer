1fd4b0d4f3cd11c39ba6de1465ad923d64e5dde1
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-23 12:31:02 -0600

    Refactor: change letters_hash keys from symbols to strings to match space key

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -8,15 +8,15 @@ describe Dictionary do
   end
 
   it "has attributes" do
-    expect(dictionary.letters_hash[:a][0]).to eq('0.')
-    expect(dictionary.letters_hash[:a]).to eq ['0.', '..', '..']
+    expect(dictionary.letters_hash["a"][0]).to eq('0.')
+    expect(dictionary.letters_hash["a"]).to eq ['0.', '..', '..']
   end
 
   it "can print a specified braille letter" do
-    expect(dictionary.print_braille(:a)).to eq("0.\n..\n..")
-    expect(dictionary.print_braille(:b)).to eq("0.\n0.\n..")
-    expect(dictionary.print_braille(:r)).to eq("0.\n00\n0.")
-    expect(dictionary.print_braille(:z)).to eq("0.\n.0\n00")
-    expect(dictionary.print_braille(' ')).to eq("..\n..\n..")
+    expect(dictionary.print_braille("a")).to eq("0.\n..\n..")
+    expect(dictionary.print_braille("b")).to eq("0.\n0.\n..")
+    expect(dictionary.print_braille("r")).to eq("0.\n00\n0.")
+    expect(dictionary.print_braille("z")).to eq("0.\n.0\n00")
+    expect(dictionary.print_braille(" ")).to eq("..\n..\n..")
   end
 end
