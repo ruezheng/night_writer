f157cc9752ac0cbb9ffb0227ec695fe3ce1f93e6
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-23 12:22:26 -0600

    Feat: add all letters of alphabet

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -14,5 +14,8 @@ describe Dictionary do
 
   it "can print a specified braille letter" do
     expect(dictionary.print_braille(:a)).to eq("0.\n..\n..")
+    expect(dictionary.print_braille(:b)).to eq("0.\n0.\n..")
+    expect(dictionary.print_braille(:r)).to eq("0.\n00\n0.")
+    expect(dictionary.print_braille(:z)).to eq("0.\n.0\n00")
   end
 end
