09babcba3092306917ab0f0ee6b3bed952c6fcda
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-23 19:08:17 -0600

    Feat: night_writer.rb translates english to braille

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,17 +7,11 @@ describe Dictionary do
     expect(dictionary).to be_a(Dictionary)
   end
 
-  it "has attributes" do
-    # expect(dictionary.english_to_braille.to be_a(Hash)
-    expect(dictionary.english_to_braille["a"][0]).to eq('0.')
-    expect(dictionary.english_to_braille["a"]).to eq ['0.', '..', '..']
-  end
-
-  it "can print a specified braille letter from a-z" do
-    expect(dictionary.translate("a")).to eq("0.\n..\n..")
-    expect(dictionary.translate("b")).to eq("0.\n0.\n..")
-    expect(dictionary.translate("r")).to eq("0.\n00\n0.")
-    expect(dictionary.translate("z")).to eq("0.\n.0\n00")
-    expect(dictionary.translate(" ")).to eq("..\n..\n..")
+  it "has letters" do
+    expect(dictionary.english_to_braille["a"]).to eq("0.....")
+    expect(dictionary.english_to_braille["b"]).to eq("0.0...")
+    expect(dictionary.english_to_braille["c"]).to eq("00....")
+    expect(dictionary.english_to_braille["z"]).to eq("0..000")
+    expect(dictionary.english_to_braille[" "]).to eq("......")
   end
 end
