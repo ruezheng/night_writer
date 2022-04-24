105b1ea34b4ab2f520721016211b2987ab5f6a08
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-23 19:48:16 -0600

    Test: Dictionary turns braille into english letters

--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -7,11 +7,23 @@ describe Dictionary do
     expect(dictionary).to be_a(Dictionary)
   end
 
-  it "has letters" do
-    expect(dictionary.english_to_braille["a"]).to eq("0.....")
-    expect(dictionary.english_to_braille["b"]).to eq("0.0...")
-    expect(dictionary.english_to_braille["c"]).to eq("00....")
-    expect(dictionary.english_to_braille["z"]).to eq("0..000")
-    expect(dictionary.english_to_braille[" "]).to eq("......")
+  describe ".english_to_braille" do
+    it "turns english letters into braille" do
+      expect(dictionary.english_to_braille['a']).to eq('0.....')
+      expect(dictionary.english_to_braille['b']).to eq('0.0...')
+      expect(dictionary.english_to_braille['c']).to eq('00....')
+      expect(dictionary.english_to_braille['z']).to eq('0..000')
+      expect(dictionary.english_to_braille[' ']).to eq('......')
+    end
+  end
+
+  describe ".braile_to_english" do
+    it "turns braille into english letters" do
+      expect(dictionary.braile_to_english['0.....']).to eq('a')
+      expect(dictionary.braile_to_english['0.0...']).to eq('b')
+      expect(dictionary.braile_to_english['00....']).to eq('c')
+      expect(dictionary.braile_to_english['0..000']).to eq('z')
+      expect(dictionary.braile_to_english['......']).to eq(' ')
+    end
   end
 end
