890c5db7279c50c99a2041eec4e0313e25f5d844
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-23 18:56:37 -0600

    Test: translate method can translate mumltiple english letters to braille

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -6,28 +6,8 @@ class BrailleTranslator
     Dictionary.new.english_to_braille
   end
 
-  def translate(letter)
+  def translate_letter(letter)
     english_to_braille[letter]
   end
+
 end
-
-
-# class BrailleTranslator
-#
-#   def english_to_braille
-#     Dictionary.new.english_to_braille
-#   end
-#
-#   def translate(letter)
-#     braille = ""
-#     english_to_braille.each do |key, value|
-#       if letter == key
-#         row0 = value[0]
-#         row1 = value[1]
-#         row2 = value[2]
-#         braille = "#{row0}\n" + "#{row1}\n" + "#{row2}"
-#       end
-#     end
-#     braille
-#   end
-# end
